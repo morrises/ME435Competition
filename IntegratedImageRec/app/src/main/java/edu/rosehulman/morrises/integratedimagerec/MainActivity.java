6ba7be9b0e055281f20c2791e4c88931724ac488
morrises <38666805+morrises@users.noreply.github.com>
2018-05-01 14:01:46 -0400

    Prepare 3 placeholder views

--- IntegratedImageRec/app/src/main/java/edu/rosehulman/morrises/integratedimagerec/MainActivity.java
+++ IntegratedImageRec/app/src/main/java/edu/rosehulman/morrises/integratedimagerec/MainActivity.java
@@ -1,15 +1,15 @@
 package edu.rosehulman.morrises.integratedimagerec;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper mViewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,32 +18,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mViewFlipper = findViewById(R.id.my_view_flipper);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_next) {
+//            Toast.makeText(this, "You pressed Next", Toast.LENGTH_SHORT).show();
+            mViewFlipper.showNext();
             return true;
         }
 
